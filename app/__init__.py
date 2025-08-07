6c375fe470c629b0d88e2676475a2daf4b4ae0a5
Articos11 <viniciuscaldas0003@gmail.com>
2025-08-07 11:28:10 -0300

    cors update

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     load_dotenv()
     app = Flask(__name__)
     __version__ = "0.1.0"
-    CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "https://devsoftwarefront.onrender.com"}})
 
     app.config['JWT_SECRET_KEY'] = "devsoftware2025"
     jwt = JWTManager(app)
